569a79005cbacfae7c532e649ea1db5324a4815f
Rodrigo <digo1209@gmail.com>
2023-04-04 09:48:51 -0300

    aula python, matéria while

---
+++ aula_python/ex_python.py
@@ -0,0 +1,47 @@
+'''
+ while e for são usados para fazer o laço de repetição
+while = enquato algo estiver acontecendo, faça algo
+
+'''
+'''
+num = 0
+
+while num<=1000:
+    print(f'Olá {num}')
+    num=num + 1
+print('Fim')
+
+'''
+
+
+
+
+'''
+num1 = 1
+pares = 0
+
+numero = int(input('Digite um numero: '))
+while num1 <= 10:
+    if numero % 2 == 0:
+        pares += 1
+    num1 += 1
+    numero +=1
+print(f'A quantidade de pares é {pares}')
+print(numero)
+'''
+'''
+
+contadores normalmente usam a variavel "cont"
+
+'''
+'''
+cont = 1
+while cont <= 20:
+    print(cont)
+    cont += 1
+'''
+cont1 = 1
+while cont1<= 20:
+    if cont1 % 3 == 0:
+        print(cont1)
+    cont1 += 1
